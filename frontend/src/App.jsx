423a0853b29e15d0f3fdb39bcf012e6e995e002e
Ashutosh <ashutoh182005@gmail.com>
2026-02-15 02:10:20 +0530

    Frontend Skeleton

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
+
+const App=()=>{
+  return(
+    <div>App</div>
+  )
+}
+
+export default App
